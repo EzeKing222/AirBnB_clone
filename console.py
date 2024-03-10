c17074d262a2d480958923833c209780a8097fbe
EzeKing222 <ezeikechukwu007@gmail.com>
2024-03-10 02:18:59 +0100

    pass pycodestyle for console

--- console.py
+++ console.py
@@ -2,47 +2,48 @@
 """
 A simple line-oriented command interpreter
 """
-
 import cmd
 from typing import IO
 
 
 class HBNBCommand(cmd.Cmd):
 
+    '''
+    A simple line-oriented command interpreter.
+    '''
+    def __init__(self, completekey: str = "tab"):
         '''
-        A simple line-oriented command interpreter.
+        Instantiate a line-oriented interpreter.
+
+        Args:
+        completekey: is the readline name of a completion key
+        it defaults to the Tab key. If completekey is not None and
+        the readline module is available,
+        command completion is done automatically.
         '''
-        def __init__(self, completekey: str = "tab", stdin: IO[str] | None = None, stdout: IO[str] | None = None) -> None:
-                '''
-                Instantiate a line-oriented interpreter.
+        super().__init__(completekey)
 
-                Args:
-                completekey: is the readline name of a completion key
-                        it defaults to the Tab key. If completekey is not None and the readline module is available,
-                        command completion is done automatically.
-                stdin and stdout: specify alternate input and output file objects; if not specified,
-                        sys.stdin and sys.stdout are used.
-                '''
-                super().__init__(completekey, stdin, stdout)
+    def do_quit(self, line):
+        '''
+        Quit command to exit the program
+        '''
+        return True
 
-        def do_quit(self, line):
-                '''
-                provides the exit command to abort the interpreter.
-                '''
-                return True
+    def do_EOF(self, line):
+        '''
+        End Of File
+        '''
+        return True
 
-        def do_EOF(self, line):
-                '''
-                End Of File
-                '''
-                return True
+    def postloop(self):
+        print()
+    '''
+    #aliasing
+    #do_quit = do_exit
+    '''
 
-        def postloop(self) -> None:
-                print()
-        #aliasing
-        #do_quit = do_exit
+    prompt = '(hbnb) '
 
-        prompt = '(hbnb) '
 
 if __name__ == "__main__":
-        HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,54 +7,55 @@ from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
+    """
+    test BaseModel
+    """
+    def test_init(self):
         """
-        test BaseModel
+        test BaseModel.__init__
         """
-        def test_init(self):
-                """
-                test BaseModel.__init__
-                """
-                my_model = BaseModel()
+        my_model = BaseModel()
 
-                self.assertIsNotNone(my_model.id)
-                self.assertIsNotNone(my_model.created_at)
-                self.assertIsNotNone(my_model.updated_at)
+        self.assertIsNotNone(my_model.id)
+        self.assertIsNotNone(my_model.created_at)
+        self.assertIsNotNone(my_model.updated_at)
 
-        def test_save(self):
-                """
-                test BaseModel.save
-                """
-                my_model = BaseModel()
+    def test_save(self):
+        """
+        test BaseModel.save
+        """
+        my_model = BaseModel()
 
-                initial_updated_at = my_model.updated_at
-                current_updated_at = my_model.save()
+        initial_updated_at = my_model.updated_at
+        current_updated_at = my_model.save()
 
-                self.assertNotEqual(initial_updated_at, current_updated_at)
+        self.assertNotEqual(initial_updated_at, current_updated_at)
 
-        def test_to_dict(self):
-                """
-                test BaseModel.to_dict
-                """
-                my_model = BaseModel()
+    def test_to_dict(self):
+        """
+        test BaseModel.to_dict
+        """
+        my_model = BaseModel()
 
-                my_model_dict = my_model.to_dict()
+        my_model_dict = my_model.to_dict()
 
-                self.assertIsInstance(my_model_dict, dict)
+        self.assertIsInstance(my_model_dict, dict)
 
-                self.assertEqual(my_model_dict["__class__"], "BaseModel")
-                self.assertEqual(my_model_dict["id"], my_model.id)
-                self.assertEqual(my_model_dict["created_at"], my_model.created_at.isoformat())
-                self.assertEqual(my_model_dict["updated_at"], my_model.updated_at.isoformat())
+        self.assertEqual(my_model_dict["__class__"], "BaseModel")
+        self.assertEqual(my_model_dict["id"], my_model.id)
+        self.assertEqual(my_model_dict["created_at"], my_model.created_at.isoformat())
+        self.assertEqual(my_model_dict["updated_at"], my_model.updated_at.isoformat())
 
-        def test_str(self):
-                """
-                test BaseModel.__str__
-                """
-                my_model = BaseModel()
+    def test_str(self):
+        """
+        test BaseModel.__str__
+        """
+        my_model = BaseModel()
 
-                self.assertTrue(str(my_model).startswith("[BaseModel]"))
-                self.assertIn(my_model.id, str(my_model))
-                self.assertIn(str(my_model.__dict__), str(my_model))
+        self.assertTrue(str(my_model).startswith("[BaseModel]"))
+        self.assertIn(my_model.id, str(my_model))
+        self.assertIn(str(my_model.__dict__), str(my_model))
+
 
 if __name__ == "__main__":
-        unittest.main()
+    unittest.main()
